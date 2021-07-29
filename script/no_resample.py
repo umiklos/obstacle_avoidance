b1f1fee9e3bb90881cecb36d96185dd392e352d3
horverno <horverno@gmail.com>
2021-07-29 16:46:03 +0200

    add all waypoints to the trajectory

--- script/no_resample.py
+++ script/no_resample.py
@@ -209,7 +209,7 @@ def callback_detectedobjects(data):
                         v1 = elkerules[k][3]
                         v2 = elkerules[k+1][3]
                         
-                        if k >= start_index:
+                        if k > start_index:
                             actual_len_of_avoid += line_length(x1, x2, y1, y2)
                             velocities_from_avoidance += line_length(x1,x2,v1,v2)
                             velocities.append(velocities_from_avoidance)
@@ -234,21 +234,19 @@ def callback_detectedobjects(data):
                             #first_part.append((x1,y1))
                             #vx.append((distances_for_start_point,elkerules[k][3]))
 
-                    first_part=elkerules[closest_waypoint+1:start_index,0:2]
+                    first_part=elkerules[:start_index+1,0:2]
                     #velocity_length=distances_for_start_point + original_distances[-1]
                     #vx=elkerules[closest_waypoint+1:start_index+1,3]
                     elkerules_data=np.concatenate((first_part,elkerules_points))
                     #velocity_ls = LineString(np.column_stack((original_distances,elkerules[start_index+1:len(elkerules)-1,3])))
-                    elkerules_ls = LineString(elkerules_points) 
-                    n=round(elkerules_ls.length/distance_delta)
-                    distances = np.linspace(0,elkerules_ls.length,n)
-                    distances_for_velocity = np.linspace(0,velocities[-1],n)
-
+                    #elkerules_data.insert(elkerules[-1,0:2])
+                   
+                    elkerules_data=np.vstack((elkerules_data,elkerules[-1,0:2]))
                     #new_velocities=np.interp(distances_for_velocity,original_distances,elkerules[start_index+1:len(elkerules)-1,3])
 
                     #new_velocities =([velocity_ls.interpolate(distance_v) for distance_v in distances_for_velocity])
                     #points = [elkerules_ls.interpolate(distance_ls) for distance_ls in distances]
-                    v=elkerules[closest_waypoint+2:,3]#np.concatenate((vx,new_velocities))
+                    v=elkerules[:,3]#np.concatenate((vx,new_velocities))
                     
                     
 
@@ -369,7 +367,7 @@ def pub():
                 w0.pose = pose
                 w0.twist.twist.linear.x = e[3]
                 msg_pub_lane.waypoints.append(w0)
-            
+            #print(len(elkerules))
             pub_based_waypoint_list.publish(msg_pub_lane)
             pub_new_data.publish(ma)
         
